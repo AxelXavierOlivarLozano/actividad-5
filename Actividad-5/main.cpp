3c17ec4c0d981c969936bdf6786655e2146fc075
AxelXavierOlivarLozano <A01286176@tec.mx>
2023-11-15 21:26:48 -0600

    Initial Commit

---
+++ Actividad-5/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Actividad-5
+//
+//  Created by Axel Xavier Olivar Lozano on 15/11/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
